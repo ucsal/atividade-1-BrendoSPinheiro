b558735f844962225bddf8e0fd196eacd67636f4
Brendo <brendo.spinheiro@gmail.com>
2020-03-16 17:01:09 -0300

    Programação Android - Atividade 1

--- app/src/main/java/br/com/mariojp/exercise1/MainActivity.java
+++ app/src/main/java/br/com/mariojp/exercise1/MainActivity.java
@@ -2,19 +2,38 @@ package br.com.mariojp.exercise1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String ENTRADA_USUARIO = "ENTRADA_USUARIO";
+    private TextView entradaUser;
+    private TextView saidaForUser;
+    private String mensagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState != null){
+            mensagem = savedInstanceState.getString(ENTRADA_USUARIO);
+            entradaUser = findViewById(R.id.editNome);
+            saidaForUser = findViewById(R.id.labelMensagem);
+            saidaForUser.setText("Alô, "+mensagem+"!");
+        }
     }
 
+    public void acaoBotaoCumprimentar(View v){
+        entradaUser = findViewById(R.id.editNome);
+        saidaForUser = findViewById(R.id.labelMensagem);
+        mensagem = entradaUser.getText().toString();
+        saidaForUser.setText("Alô, "+mensagem+"!");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString(ENTRADA_USUARIO, mensagem);
+        super.onSaveInstanceState(outState);
+    }
 }
