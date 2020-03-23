efd4b74d0d1126d34a8cf564c3da21a1600ccb01
Brendo <brendo.spinheiro@gmail.com>
2020-03-23 17:08:13 -0300

    UPDATE: code refactoring

--- app/src/main/java/br/com/mariojp/exercise1/MainActivity.java
+++ app/src/main/java/br/com/mariojp/exercise1/MainActivity.java
@@ -8,25 +8,22 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String ENTRADA_USUARIO = "ENTRADA_USUARIO";
-    private TextView entradaUser;
-    private TextView saidaForUser;
+    private TextView entradaUser, saidaForUser;
     private String mensagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        entradaUser = findViewById(R.id.editNome);
+        saidaForUser = findViewById(R.id.labelMensagem);
         if (savedInstanceState != null){
             mensagem = savedInstanceState.getString(ENTRADA_USUARIO);
-            entradaUser = findViewById(R.id.editNome);
-            saidaForUser = findViewById(R.id.labelMensagem);
             saidaForUser.setText("Alô, "+mensagem+"!");
         }
     }
 
     public void acaoBotaoCumprimentar(View v){
-        entradaUser = findViewById(R.id.editNome);
-        saidaForUser = findViewById(R.id.labelMensagem);
         mensagem = entradaUser.getText().toString();
         saidaForUser.setText("Alô, "+mensagem+"!");
     }
